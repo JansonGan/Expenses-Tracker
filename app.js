7550a763f7b5c8799ce61e2236b6de08c9ffc757
JansonGan <janson.gbc@gmail.comgit config --global user.name>
2022-02-08 22:24:32 +0800

    Update port

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ Date.prototype.getWeek = function() {
     // Adjust to Thursday in week 1 and count number of weeks from date to week1.
     return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000- 3 + (week1.getDay() + 6) % 7) / 7);
   };
-    
+
 app.get("/", function (req, res) {
 
     Expenses.find({}, function (err, itemFound) {
@@ -48,7 +48,7 @@ app.get("/", function (req, res) {
                 }
                 if(item.date.getMonth() === new Date().getMonth()){
                     monthlyExpenses += item.amount;
-    
+
                  }
                  if(item.date.getYear() === new Date().getYear()){
                     yearlyExpenses += item.amount;
@@ -58,7 +58,7 @@ app.get("/", function (req, res) {
                 }
             });
 
-           
+
         }
 
         res.render("index", {
